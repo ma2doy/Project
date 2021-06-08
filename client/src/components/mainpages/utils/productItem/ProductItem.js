d8a88dc6c752f85965d3578b306317bb54e9f8a7
ma2doy <youssef.hamrouni999@gmail.com>
2021-06-08 17:17:53 +0100

    first commit

---
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import BtnRender from './BtnRender'
+
+function ProductItem({product, isAdmin, deleteProduct, handleCheck}) {
+
+    return (
+        <div className="product_card">
+            <img src={product.images.url} alt=""/>
+            
+            <div className="product_box">
+                <h2 title={product.title}>{product.title}</h2>
+                <span>${product.price}</span>
+                <p>{product.description}</p>
+            </div>
+
+            
+            <BtnRender product={product} deleteProduct={deleteProduct} />
+        </div>
+    )
+}
+
+export default ProductItem
